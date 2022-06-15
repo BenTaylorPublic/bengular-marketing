94d0cbc50da0a2e1280cf9294a103f83b37ee7f3
Ben Taylor <Benjaminjacktaylor@hotmail.com>
2022-06-15 22:14:08 +1000

    More styling changes

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,6 +1,27 @@
 class IndexView {
+    private static isMobile: boolean = false;
 
     public static initialize(): void {
+        this.detectMobiles();
+        this.setKeyFeatureHeight();
+    }
+
+    private static detectMobiles(): void {
+        if (window.matchMedia("only screen and (max-width: 760px)").matches) {
+            //Conditional script here
+            this.isMobile = true;
+            document.body.classList.add("mobile");
+        }
+    }
+
+    private static setKeyFeatureHeight(): void {
+        if (!this.isMobile) {
+            return;
+        }
+        const keyFeatures: HTMLElement[] = Array.from(<HTMLElement[]><any>document.getElementsByClassName("keyFeature"));
+        for (const keyFeature of keyFeatures) {
+            keyFeature.style.height = window.innerHeight + "px";
+        }
     }
 }
 
